8f786d3bb3683f0844f0ed9548c660cce5546857
yn-coder <yn.coder@gmail.com>
2026-06-22 00:40:48 +0300

    #6 question_description changed to question_title

--- app/pop.py
+++ app/pop.py
@@ -8,17 +8,17 @@ CONST_QUESTS = [
     { "name" : "Do you know the planets?",
       "description" : "Do you know the planets of Solar system?",
       "questions" : [
-          { "question_description" : "Which planet from the Sun is Earth?", 
+          { "question_title" : "Which planet from the Sun is Earth?", 
             "answers" : [ 
               [ "(2) Second", None, False, "No, second planet is a Venus." ], 
               [ "(3) Third", "Yes", True, None ], 
               [ "(4) Fourth", None, False, "No, fourth planet is a Mars." ], 
                
               ] },
-          { "question_description" : "Moon", "answers" : [ [ "Is it a natural satellite?", "Yes", True, "No" ] ] } ]
+          { "question_title" : "Moon", "answers" : [ [ "Is it a natural satellite?", "Yes", True, "No" ] ] } ]
       },
 
-    { "name" : "Do you know the Roman Empire?", "description" : "Do you know the history of ancient Roman Empire?", "questions" : [ { "question_description" : "Trajan" }, { "question_description" : "Marcus Aurelius" }, ] }
+    { "name" : "Do you know the Roman Empire?", "description" : "Do you know the history of ancient Roman Empire?", "questions" : [ { "question_title" : "Trajan" }, { "question_title" : "Marcus Aurelius" }, ] }
  ]
 
 async def pop_data( DB : AsyncSession ):
@@ -34,7 +34,7 @@ async def pop_data( DB : AsyncSession ):
         DB.add( quest )
         await DB.commit()
         for qd in сq_d["questions"]:
-            q = Question( question_description = qd["question_description"], quest_id = quest.id )
+            q = Question( question_title = qd["question_title"], quest_id = quest.id )
             DB.add( q )
             await DB.commit()
             if qd.get("answers"):
