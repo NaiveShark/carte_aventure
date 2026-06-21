fa453eb06dc254bfda8a019c9408d6f48ac42604
yn-coder <yn.coder@gmail.com>
2026-06-21 15:48:17 +0300

    #4 basic auth, user is db object

--- app/models.py
+++ app/models.py
@@ -1,15 +1,81 @@
 from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
-from app.config import Base
 
-class User(Base):
-    __tablename__ = "user"
+# Auth and user tables
+from passlib.hash import pbkdf2_sha256
+from sqlalchemy import Boolean, Column, Integer, String, select
+from sqlalchemy.ext.asyncio import AsyncSession
+from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from starlette.requests import Request
+from starlette_login.mixins import UserMixin
 
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    is_active = Column(Boolean, default=True)
+
+@as_declarative()
+class Base:
+    __name__: str
+
+    @declared_attr
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower()
+
+
+class User(Base, UserMixin):
+    id = Column(Integer, primary_key=True)
+    username = Column(String(150), unique=True)
+    password = Column(String(128))
+    is_admin = Column(Boolean, default=False)
+    is_editor = Column(Boolean, default=False)
+
+    @property
+    def identity(self):
+        return self.id
+
+    @property
+    def display_name(self) -> str:
+        return ' '.join([self.first_name, self.last_name])
+
+    def set_password(self, password: str):
+        self.password = pbkdf2_sha256.hash(password)
+
+    def check_password(self, password: str):
+        return pbkdf2_sha256.verify(password, self.password)
+
+    @classmethod
+    async def get_user_by_id(cls, request: Request, user_id: int):
+        db = request.state.db
+        return await db.get(User, user_id)
+
+    @classmethod
+    async def get_user_by_username(cls, db: AsyncSession, username: str):
+        query = select(User).where(User.username == username)
+        result = await db.execute(query)
+        if result is None:
+            return None
+        else:
+            return result.scalars().first()
+
+    @classmethod
+    async def create_user(
+        cls, db: AsyncSession,
+        username: str, password: str, 
+        is_admin: bool = False,
+        is_editor: bool = False
+    ):
+        user = cls(
+            username=username,
+            password=password,
+            is_admin=is_admin,
+            is_editor=is_editor
+        )
+        user.set_password(password)
+        db.add(user)
+        await db.commit()
+        return user
+
+
+
+# Data tables
 
 class Quest(Base):
     __tablename__ = "quest"
